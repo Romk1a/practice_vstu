b3d5c96f966f1c5921ece553f11fa9cce5290db4
rznkivan <rzivan1406@icloud.com>
2024-07-02 22:56:53 +0300

    Update parallelogramm.h

--- parallelogramm.h
+++ parallelogramm.h
@@ -3,13 +3,10 @@
 
 class Parallelogram {
 public:
-    // Конструкторы
-    Parallelogram();
     Parallelogram(double a, double b, double alpha);
     Parallelogram(double a, double b, double c, double d);
     Parallelogram(double alpha, double beta, double a, double b);
 
-    // Геттеры и сеттеры
     double getSideA() const;
     double getSideB() const;
     double getSideC() const;
@@ -18,10 +15,7 @@ public:
     double getAngleB() const;
     double getAngleC() const;
     double getAngleD() const;
-    void setSides(double a, double b, double c, double d);
-    void setAngles(double alpha, double beta, double gamma, double delta);
 
-    // Методы вычислений
     double calculateArea() const;
     double calculatePerimeter() const;
     double calculateSide(int index) const;
